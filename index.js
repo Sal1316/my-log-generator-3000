a4d062458c0211fd4e7d99df6730967908f414ef
Sal Ceja <salceja45@gmail.com>
2023-09-13 14:04:04 -0700

    done

--- index.js
+++ index.js
@@ -1,74 +1,50 @@
-const Shape = require('./lib/shape')
+const { Circle, Square, Triangle } = require('./lib/shapes')
 const fs = require("fs");
 const inquirer = require("inquirer");
 const chalk = require('chalk');
-
 
 const questions = [
       {
             type: "input",
             name: "text",
-            message: chalk.green("Enter up to 3 characters")
+            message: chalk.green("Enter up to 3 characters"),
+            default: 'ABC'
+
       },
       {
             type: "list",
             name: "textColor",
             message: chalk.green("Choose the text color!"),
-            choices: ["red", "yellow", "green"]
+            choices: ["red", "yellow", "green"],
+            default: 'green'
       },
       {
             type: "list",
             name: "shape",
             message: chalk.green("Choose the shape!"),
-            choices: ["circle", "rectangle", "triangle"]
+            choices: ["circle", "rectangle", "triangle"],
+            default: 'rectangle'
       },
       {
             type: "list",
             name: "shapeColor",
             message: chalk.green("Choose the shape color!"),
-            choices: ["red", "yellow", "green"]
+            choices: ["red", "yellow", "green"],
+            default: 'yellow'
       }
 ]
-const generateSvg = ({ text, textColor, shape, shapeColor }) => {
-      let shapeReasigned = '';
 
-      if (shape === 'rectangle') {
-            shapeReasigned = 'rect';
-      }
-      else if (shape === 'triangle') {
-            shapeReasigned = 'polygon';
-      } else {
-            shapeReasigned = 'circle';
-      }
-      console.log("shape === ", shape)
+function generateSvg() {
+      inquirer.prompt(questions).then(({ text, textColor, shape, shapeColor }) => {
 
-      if (shape === 'circle') {
-            return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-      <${shapeReasigned} cx="150" cy="100" r="80" fill="${shapeColor}" />
-      <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
-</svg>
-`
-      } else if (shape === 'rectangle') { // square
-            return `<svg version="1.1" width="300" height="300" xmlns="http://www.w3.org/2000/svg">
-      <${shapeReasigned} x="50" y="50" width="200" height="200" fill="${shapeColor}" />
-      <text x="150" y="170" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
-</svg>
-`
-      } else { // triangle
-            return `<svg version="1.1" width="900" height="500" xmlns="http://www.w3.org/2000/svg">
-      <${shapeReasigned} points="200,150 80,380 320,380" fill="${shapeColor}" />
-      <text x="200" y="325" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
-</svg>
-`
-      }
-}
+            let Shape = 
+                  shape == "circle" ? 
+                        Circle : 
+                  shape == "triangle" ?
+                        Triangle :
+                  Square;
 
-function init() {
-      inquirer.prompt(questions).then((answers) => {
-            console.log("Answers: ", answers);
-            // const svgTemplate = generateSvg(answers);
-
-            const shapeInstance = new Shape(text, textColor, shape, shapeColor);
+            const shapeInstance = new Shape(text, textColor, shapeColor);
             const svgTemplate = shapeInstance.render();
 
             const fileName = "./examples/logo.svg";
@@ -78,4 +54,4 @@ function init() {
       });
 }
 
-init();
+generateSvg();
